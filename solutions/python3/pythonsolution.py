ed690bb62a86f01e5ca86bf0ca072e8da9b13bed
Anooj <anoojgandham@gmail.com>
2021-07-23 19:35:26 +0530

    testharness

--- solutions/python3/pythonsolution.py
+++ solutions/python3/pythonsolution.py
@@ -38,4 +38,16 @@ class Solution:
         except :
             return "Check the format of input variables"
 
+    def reverse(self, nums,start,end):
+        while start < end:
+            nums[start],nums[end] = nums[end],nums[start]
+            start,end = start+1,end-1
 
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k %= n
+        
+        self.reverse(nums,0,n-1)
+        self.reverse(nums,0,k-1)
+        self.reverse(nums,k,n-1)
+        # nums[:] = nums[-k:] + nums[:-k]
