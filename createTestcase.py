fb15a3667f46ec2b1249e9ad4562bec1bb7ba062
Anooj <anoojgandham@gmail.com>
2021-07-29 23:58:44 +0530

    added changes to testcase generator

--- createTestcase.py
+++ createTestcase.py
@@ -8,17 +8,19 @@ problem_keyword = sys.argv[1]
 
 def genArgUsingDtype(d,type):
     # Read the datatype and generate random testcase
-    length = random.randint(3,50)
-    if d == "string":
+    if d == "string": # generate a random string of length between 3 and 50 
+        length = random.randint(3,50)
         return ''.join(random.choice(string.ascii_letters) for m in range(length))
-    elif d == "integer":
+    elif d == "integer": # integer numbers
         return random.randint(0,50)
-    elif d == "array":
-        if type['items']['type'] == 'number':
+    elif d == "array": 
+        if type['items']['type'] == 'number': # generate a random array
             return list(np.random.randint(-50,50,random.randint(1,50)))
-    elif d == "number":
+    elif d == "number": #float numbers
         return random.random()*random.randint(-100,100)
-
+    elif d == "matrix": # generate a square matrix of size matrixSize
+        matrixSize = random.randint(1,15)
+        return np.random.randint(0,2,(matrixSize,matrixSize))
 
 testcases = open(f'./testcases/{problem_keyword}.json')
 testcases = json.load(testcases)
@@ -28,13 +30,16 @@ testcases = json.load(testcases)
 inputArgs = list(testcases['testcases'][0]['args'].keys())
 schema = testcases['schema']
 args = testcases['testcases'][0]['args'].copy()
-print(inputArgs)
+
 if 'checkUnorderedLists' in inputArgs:
     inputArgs.remove('checkUnorderedLists')
-for i in inputArgs:
+for i in inputArgs: 
+    """
+    pass all the input args types to the generator 
+    function and get random testcase for input argument
+    """
     dtype = schema['properties']['args']['properties'][i]['type']
     arg = genArgUsingDtype(dtype,schema['properties']['args']['properties'][i])
     args[i] = arg
 
-# print(dtype)
 print(args)
