70bf17b27703913cb52c1282490aff8b7be27252
Sahil0128 <74017020+Sahil0128@users.noreply.github.com>
2022-05-16 22:25:47 +0530

    Update main.py

--- May2022/LongestCommonPrefix/main.py
+++ May2022/LongestCommonPrefix/main.py
@@ -3,10 +3,8 @@ cp=0
 cf=0
 for j in range(n):
     with open("testcases/Lcp" + str(j+1) + ".txt") as f:
-    content =f.readlines()
-    s1=[x.strip() for x in content]
-    #print(s1)
-    #print(l1)
+        content =f.readlines()
+        s1=[x.strip() for x in content]
     roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
     def solutionGold(strs):
         if len(strs) == 0:
