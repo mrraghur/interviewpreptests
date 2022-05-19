1b47922848f9d497c414cb69555910b6e7205bd7
Parzival17 <52356527+Prashanthveddula@users.noreply.github.com>
2022-05-19 19:10:32 +0530

    Created common.py

---
+++ May2022/ConvertSortedArraytoBST/common.py
@@ -0,0 +1,15 @@
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    
+
+def count_cases(cp, cf):
+	
+	print("The number of test cases passed: ", cp)
+	print("The number of test cases failed: ", cf)
+
+
