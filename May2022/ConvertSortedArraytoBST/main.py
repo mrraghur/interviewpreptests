b01c843a64dc6c8616596c12587c4916ff7de5d4
Parzival17 <52356527+Prashanthveddula@users.noreply.github.com>
2022-05-19 19:09:27 +0530

    Made suggested changes to main.py
    Added try except block
    Changed the variable names
    Added the two methods under the same class
    Imported treenode class from common.py

--- May2022/ConvertSortedArraytoBST/main.py
+++ May2022/ConvertSortedArraytoBST/main.py
@@ -1,48 +1,52 @@
+import common
+
+
 n = 10  # number of testcases
-cp = 0
-cf = 0
-for j in range(n):
-    with open("testcases/ArrtoBST" + str(j + 1) + ".txt") as f:
-        content = f.readlines()
-        arr = [int(x) for x in content]
+CountPassed = 0
+CountFailed = 0
 
-    class TreeNode:
-        def __init__(self, val=0, left=None, right=None):
-            self.val = val
-            self.left = left
-            self.right = right
-
-    class Solution:
+class Solution:
         def solutionGold(self, num):
             if not num:
                 return None
 
             mid = len(num) // 2
 
-            root = TreeNode(num[mid])
+            root = common.TreeNode(num[mid])
             root.left = self.solutionGold(num[:mid])
             root.right = self.solutionGold(num[mid + 1 :])
 
             return root
 
+        def soluser(self, nums):
+            # wrong solution
+            x = []
+            k = -2
+            return k
+
+
+for j in range(n):
+    try:
+        with open("testcases/ArrtoBST" + str(j + 1) + ".txt") as f:
+            content = f.readlines()
+            arr = [int(x) for x in content]
+    except:
+        print('Invalid testcase')
+    
+
+
     obj = Solution()
     ans = obj.solutionGold(arr)
+    user_ans = obj.soluser(arr)
 
-    def soluser(nums):
-        # wrong solution
-        x = []
-        k = -2
-        return k
-
-    u = soluser(arr)
-
-    if ans == u:
-        cp += 1
-        print("YES")
+    if ans == user_ans:
+        CountPassed += 1
+        print("Passed, testcase-", j+1)
     else:
-        cf += 1
-        print("NO")
+        CountFailed += 1
+        print("Failed, testcase-", j+1)
 
 
-print("The number of test cases passed: ", cp)
-print("The number of test cases failed: ", cf)
+common.count_cases(CountPassed, CountFailed)
+
+
