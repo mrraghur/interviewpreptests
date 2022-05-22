35ce6e387f318b25c68a67f5fafd5b04b550d6a9
Parzival17 <52356527+Prashanthveddula@users.noreply.github.com>
2022-05-23 00:37:47 +0530

    Made suggest changes to main.py and common.py

--- May2022/ConvertSortedArraytoBST/common.py
+++ May2022/ConvertSortedArraytoBST/common.py
@@ -1,3 +1,47 @@
+<<<<<<< HEAD
+from binarytree import bst, build
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+countPassed, countFailed = 0, 0
+
+
+def check(ans,userAns,tescaseNumber):
+    global countPassed, countFailed
+
+    if ans == userAns:
+        countPassed += 1
+        print("Passed, testcase-", tescaseNumber)
+    else:
+        countFailed += 1
+        print("Failed, testcase-", tescaseNumber)
+    return countPassed,countFailed
+
+
+def count_cases():
+	
+	print("The number of test cases passed: ", countPassed)
+	print("The number of test cases failed: ", countFailed)
+
+    
+def generate_bst(k):
+    root = bst(height=k)
+    return root.values
+
+
+def build_bst(values):
+    root = build(values)
+    return root
+
+
+
+
+=======
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -13,3 +57,4 @@ def count_cases(cp, cf):
 	print("The number of test cases failed: ", cf)
 
 
+>>>>>>> 1b47922848f9d497c414cb69555910b6e7205bd7
--- May2022/ConvertSortedArraytoBST/main.py
+++ May2022/ConvertSortedArraytoBST/main.py
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
+from common import *
+
+n = 10  # number of testcases
+
+
+=======
 import common
 
 
@@ -5,6 +12,7 @@ n = 10  # number of testcases
 CountPassed = 0
 CountFailed = 0
 
+>>>>>>> 1b47922848f9d497c414cb69555910b6e7205bd7
 class Solution:
         def solutionGold(self, num):
             if not num:
@@ -26,12 +34,29 @@ class Solution:
 
 
 for j in range(n):
+<<<<<<< HEAD
+    testcaseNumber = j+1
+    try:
+        with open("BuiltbyInterns/interviewpreptests/May2022/ConvertSortedArraytoBST/testcases/ArrtoBST" + str(j + 1) + ".txt") as f:
+=======
     try:
         with open("testcases/ArrtoBST" + str(j + 1) + ".txt") as f:
+>>>>>>> 1b47922848f9d497c414cb69555910b6e7205bd7
             content = f.readlines()
             arr = [int(x) for x in content]
     except:
         print('Invalid testcase')
+<<<<<<< HEAD
+
+    
+
+    obj = Solution()
+    ans = obj.solutionGold(arr)
+    userAns = obj.soluser(arr)
+    check(ans, userAns, testcaseNumber)
+
+count_cases()
+=======
     
 
 
@@ -48,5 +73,6 @@ for j in range(n):
 
 
 common.count_cases(CountPassed, CountFailed)
+>>>>>>> 1b47922848f9d497c414cb69555910b6e7205bd7
 
 
