b3c5c0f81ce9dee1a7d48da9187e710a5cc5922d
Parzival17 <52356527+Prashanthveddula@users.noreply.github.com>
2022-05-23 00:44:54 +0530

    updated main.py

--- May2022/ConvertSortedArraytoBST/main.py
+++ May2022/ConvertSortedArraytoBST/main.py
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
 from common import *
 
 n = 10  # number of testcases
 
-
-=======
-import common
-
-
-n = 10  # number of testcases
-CountPassed = 0
-CountFailed = 0
-
->>>>>>> 1b47922848f9d497c414cb69555910b6e7205bd7
 class Solution:
         def solutionGold(self, num):
             if not num:
@@ -20,7 +9,7 @@ class Solution:
 
             mid = len(num) // 2
 
-            root = common.TreeNode(num[mid])
+            root = TreeNode(num[mid])
             root.left = self.solutionGold(num[:mid])
             root.right = self.solutionGold(num[mid + 1 :])
 
@@ -34,19 +23,15 @@ class Solution:
 
 
 for j in range(n):
-<<<<<<< HEAD
     testcaseNumber = j+1
     try:
-        with open("BuiltbyInterns/interviewpreptests/May2022/ConvertSortedArraytoBST/testcases/ArrtoBST" + str(j + 1) + ".txt") as f:
-=======
-    try:
         with open("testcases/ArrtoBST" + str(j + 1) + ".txt") as f:
->>>>>>> 1b47922848f9d497c414cb69555910b6e7205bd7
+
             content = f.readlines()
             arr = [int(x) for x in content]
     except:
         print('Invalid testcase')
-<<<<<<< HEAD
+
 
     
 
@@ -56,23 +41,6 @@ for j in range(n):
     check(ans, userAns, testcaseNumber)
 
 count_cases()
-=======
-    
 
-
-    obj = Solution()
-    ans = obj.solutionGold(arr)
-    user_ans = obj.soluser(arr)
-
-    if ans == user_ans:
-        CountPassed += 1
-        print("Passed, testcase-", j+1)
-    else:
-        CountFailed += 1
-        print("Failed, testcase-", j+1)
-
-
-common.count_cases(CountPassed, CountFailed)
->>>>>>> 1b47922848f9d497c414cb69555910b6e7205bd7
 
 
