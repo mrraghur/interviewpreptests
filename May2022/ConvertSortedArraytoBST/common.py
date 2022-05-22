012541a204b4eb2adafba00ba5b3fe4b819b1628
Parzival17 <52356527+Prashanthveddula@users.noreply.github.com>
2022-05-23 00:44:33 +0530

    Updated main.py

--- May2022/ConvertSortedArraytoBST/common.py
+++ May2022/ConvertSortedArraytoBST/common.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from binarytree import bst, build
 
 
@@ -41,20 +40,4 @@ def build_bst(values):
 
 
 
-=======
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-    
-
-def count_cases(cp, cf):
-	
-	print("The number of test cases passed: ", cp)
-	print("The number of test cases failed: ", cf)
-
-
->>>>>>> 1b47922848f9d497c414cb69555910b6e7205bd7
