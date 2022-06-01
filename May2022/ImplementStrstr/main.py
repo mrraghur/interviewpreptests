d57686541b52e9e797c36cf0bab29871ad0d2980
Sahil0128 <74017020+Sahil0128@users.noreply.github.com>
2022-06-01 18:10:04 +0530

    Updated main.py
    Added try, except and put class Solution.

--- May2022/ImplementStrstr/main.py
+++ May2022/ImplementStrstr/main.py
@@ -1,32 +1,28 @@
+from common import *
 n = 10  # number of testcases
-cp=0
-cf=0
+class Solution:
+        def solutionGold(haystack,needle):
+            if needle == '':
+                return 0
+                length = len(needle)
+                for i in range(len(haystack) - length + 1):
+                    if haystack[i:i + length] == needle:
+                        return i
+            return -1
+        def soluser(haystack,needle):
+            x = []
+            k = -2
+            return k;
 for j in range(n):
-    with open("testcases/is" + str(j+1) + ".txt") as f:
-        content =f.readlines()
-        l1=[x.strip() for x in content]
-    #print(l1)
-    def solutionGold(haystack,needle):
-        if needle == '':
-            return 0
-        length = len(needle)
-        for i in range(len(haystack) - length + 1):
-            if haystack[i:i + length] == needle:
-                return i
-        return -1
-    ans=solutionGold(l1[0],l1[1])
-    def soluser(haystack,needle):
-        #wrong solution
-        x=[]
-        k=-2
-        return k;
-    u=soluser(l1[0],l1[1])
-    if ans==u:
-        cp+=1
-        print('YES')
-    else:
-        cf+=1
-        print('NO')
-    #print(ans)
-print("The number of test cases passed: ",cp)
-print("The number of test cases failed: ",cf)
+    testcaseNumber = j+1
+    try:
+        with open("testcases/is" + str(j+1) + ".txt") as f:
+            content =f.readlines()
+            l1=[x.strip() for x in content]
+    except:
+        print('Invalid testcase', testcaseNumber)
+    obj = Solution()
+    ans= obj.solutionGold(l1[0],l1[1])
+    userAns = obj.soluser(l1[0],l1[1])
+    check(ans,userAns,testcaseNumber)
+count_cases()
