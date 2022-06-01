535c17c994e9551021852fbd3064d0592cb7a054
Sahil0128 <74017020+Sahil0128@users.noreply.github.com>
2022-06-01 17:21:00 +0530

    Update common.py

--- May2022/UniqueBSTs/common.py
+++ May2022/UniqueBSTs/common.py
@@ -1,3 +1,15 @@
+from binarytree import bst, build
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+countPassed, countFailed = 0, 0
+
+
 def check(ans,userAns,tescaseNumber):
     global countPassed, countFailed
 
@@ -8,7 +20,25 @@ def check(ans,userAns,tescaseNumber):
         countFailed += 1
         print("Failed, testcase-", tescaseNumber)
     return countPassed,countFailed
-  
+
+
 def count_cases():
+	
 	print("The number of test cases passed: ", countPassed)
 	print("The number of test cases failed: ", countFailed)
+
+
+
+def build_bst(values):
+    root = build(values)
+    return root
+
+def read_bst(arr):
+    res = []
+    for i in arr:
+        if i >= str(0):
+            res.append(int(i))
+        else:
+            res.append(None)
+
+    return res
