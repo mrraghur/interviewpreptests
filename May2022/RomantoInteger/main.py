593f6096195457dd8e1308b5eb0d61231036dd3e
Sahil0128 <74017020+Sahil0128@users.noreply.github.com>
2022-06-01 17:08:24 +0530

    Updated main.py
    Added try, except and put class Solution.

--- May2022/RomantoInteger/main.py
+++ May2022/RomantoInteger/main.py
@@ -1,32 +1,29 @@
+from common import *
 n = 10  # number of testcases
-
+roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+class Solution:
+        def solutionGold(S):
+            summ = 0
+            for i in range(len(S) - 1, -1, -1):
+                num = roman[S[i]]
+                if 3 * num < summ:
+                    summ = summ - num
+                else:
+                    summ = summ + num
+            return summ
+        def soluser(S):
+            x = []
+            k = -2
+            return k;
 for j in range(n):
-
-    with open("testcases/rmn" + str(j + 1) + ".txt") as f:
-        rmn = f.read()
-
-    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-
-    def solutionGold(S):
-        summ = 0
-        for i in range(len(S) - 1, -1, -1):
-            num = roman[S[i]]
-            if 3 * num < summ:
-                summ = summ - num
-            else:
-                summ = summ + num
-        return summ
-
-    ans = solutionGold(rmn)
-
-    def solUser(S):  # wrong solution
-        x = []
-        k = 2
-        return k
-
-    u = solUser(rmn)
-
-    if ans == u:
-        print("Correct answer!, Testcase-" + str(j + 1))
-    else:
-        print("Wrong answer!, Testcase-" + str(j + 1))
+    testcaseNumber = j+1
+    try:
+        with open("testcases/rmn" + str(j + 1) + ".txt") as f:
+            rmn = f.read()
+    except:
+        print('Invalid testcase', testcaseNumber)
+    obj = Solution()
+    ans = obj.solutionGold(rmn)
+    userAns = obj.soluser(rmn)
+    check(ans, userAns, testcaseNumber)
+count_cases()
