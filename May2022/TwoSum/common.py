aef9ec57985cbfe222e30c9b3ed18b7c37549fc3
Sahil0128 <74017020+Sahil0128@users.noreply.github.com>
2022-06-01 17:20:15 +0530

    Update common.py

--- May2022/TwoSum/common.py
+++ May2022/TwoSum/common.py
@@ -1,3 +1,15 @@
+from binarytree import bst, build
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+countPassed, countFailed = 0, 0
+
+
 def check(ans,userAns,tescaseNumber):
     global countPassed, countFailed
 
@@ -14,3 +26,19 @@ def count_cases():
 	
 	print("The number of test cases passed: ", countPassed)
 	print("The number of test cases failed: ", countFailed)
+
+
+
+def build_bst(values):
+    root = build(values)
+    return root
+
+def read_bst(arr):
+    res = []
+    for i in arr:
+        if i >= str(0):
+            res.append(int(i))
+        else:
+            res.append(None)
+
+    return res
