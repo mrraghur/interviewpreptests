55eb53f561e9fbc06ab6a8797cee698f633c0885
Sahil0128 <74017020+Sahil0128@users.noreply.github.com>
2022-06-01 18:00:29 +0530

    Updated main.py
    Added try, except and put class Solution.

--- May2022/LongestCommonPrefix/main.py
+++ May2022/LongestCommonPrefix/main.py
@@ -1,38 +1,34 @@
+from common import *
 n = 10  # number of testcases
-cp=0
-cf=0
+class Solution:
+        def solutionGold(strs):
+            if len(strs) == 0:
+                return ''
+            elif len(strs) == 1:
+                return strs[0]
+            m = len(min(strs, key=len))
+            i = 0
+            reference = strs[0]
+            while i < m:
+                for myStr in strs:
+                    if myStr[i] != reference[i]:
+                        return reference[:i]
+                i += 1
+            return reference[:m]
+        def soluser(strs):
+            x = []
+            k = ""
+            return k;
 for j in range(n):
-    with open("testcases/Lcp" + str(j+1) + ".txt") as f:
-        content =f.readlines()
-        s1=[x.strip() for x in content]
-    roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-    def solutionGold(strs):
-        if len(strs) == 0:
-            return ''
-        elif len(strs) == 1:
-            return strs[0]
-        m = len(min(strs, key=len))
-        i = 0
-        reference = strs[0]
-        while i < m:
-            for myStr in strs:
-                if myStr[i] != reference[i]:
-                    return reference[:i]
-            i += 1
-        return reference[:m]
-    ans=solutionGold(s1)
-    def soluser(strs):
-        #wrong solution 
-        x=[]
-        k=""
-        return k;
-    u=soluser(s1)
-    if ans==u:
-        cp+=1
-        print('YES')
-    else:
-        cf+=1
-        print('NO')
-    #print(ans)
-print("The number of test cases passed: ",cp)
-print("The number of test cases failed: ",cf)
+    testcaseNumber = j+1
+    try:
+        with open("testcases/Lcp" + str(j+1) + ".txt") as f:
+            content =f.readlines()
+            s1=[x.strip() for x in content]
+    except:
+        print('Invalid testcase', testcaseNumber)
+    obj = Solution()
+    ans= obj.solutionGold(s1)
+    userAns = obj.soluser(s1)
+    check(ans,userAns,testcaseNumber)
+count_cases()
