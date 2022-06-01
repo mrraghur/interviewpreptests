85823c56b16d4a5bb7521b9f00d85ccc9f91d9a3
Prashanth <prashanthveddula777@gmail.com>
2022-06-01 16:07:09 +0530

    Made changes to ValidParentheses

--- May2022/ValidParentheses/main.py
+++ May2022/ValidParentheses/main.py
@@ -1,31 +1,43 @@
+from common import *
+
+
 n = 10  # number of testcases
-cp=0
-cf=0 
+
+
+class Solution:
+        def solutionGold(self, s):
+            pair = dict(('()', '[]', '{}'))
+            st = []
+            for x in s:
+                if x in '([{':
+                    st.append(x)
+                elif len(st) == 0 or x != pair[st.pop()]:
+                    return False
+            return len(st) == 0
+
+        def soluser(self, s):
+            # wrong solution
+            x = []
+            k = -2
+            return k
+
+
 for j in range(n):
-    with open("testcases/vp" + str(j+1) + ".txt") as f:
-        s1=f.read()
-    def solutionGold(s):
-        pair = dict(('()', '[]', '{}'))
-        st = []
-        for x in s:
-            if x in '([{':
-                st.append(x)
-            elif len(st) == 0 or x != pair[st.pop()]:
-                return False
-        return len(st) == 0
-    ans=solutionGold(s1)
-    def soluser(s):
-        #wrong solution
-        x=[]
-        k=2
-        return False;
-    u=soluser(s1)
-    if ans==u:
-        cp+=1
-        print('YES')
-    else:
-        cf+=1
-        print('NO')
-    #print(ans)
-print("The number of test cases passed: ",cp)
-print("The number of test cases failed: ",cf)
+    testcaseNumber = j+1
+    try:
+        #Input values to build the binary search tree
+        with open("testcases/vp" + str(j + 1) + ".txt") as f:
+            inp = f.read()
+
+    except:
+        print('Invalid testcase', testcaseNumber)
+
+
+    obj = Solution()
+    ans = obj.solutionGold(inp)
+    userAns = obj.soluser(inp)
+    check(ans, userAns, testcaseNumber)
+
+count_cases()
+
+
