8cd4d614687c2189549ee7893db779a9fdf16cf3
Sahil0128 <74017020+Sahil0128@users.noreply.github.com>
2022-07-07 23:10:56 +0530

    Add files via upload

---
+++ May2022/PlusOne/main.py
@@ -0,0 +1,33 @@
+from typing import List
+from common import *
+n = 10  # number of testcases
+class Solution:
+        def solutionGold(self, digits: List[int]) -> List[int]:
+            res = int("".join(map(str, digits)))
+            res = [int(x) for x in str(res+1)]
+            return res
+        def soluser(self, num):
+            x = []
+            k = -2
+            return x
+
+for j in range(n):
+    testcaseNumber = j+1
+    try:
+        with open("testcases/po" + str(j+1) + ".txt") as f:
+            #num=int(f.read())
+            content =f.readlines()
+            num=[x.strip() for x in content]
+            #if num.length()<3 or num.length()>1000:
+                #print("Please enter an array of size in range of [3,1000]; testcase-",testcaseNumber)
+        
+    except:
+        print('Invalid testcase', testcaseNumber)
+        continue
+     
+    obj = Solution()
+    ans = obj.solutionGold(num)
+    userAns = obj.soluser(num)
+    check(ans, userAns, testcaseNumber)
+
+count_cases()
