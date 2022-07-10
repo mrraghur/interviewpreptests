cd304238cb78ccb70f74b683761db66956139562
Sahil0128 <74017020+Sahil0128@users.noreply.github.com>
2022-07-11 01:43:50 +0530

    Add files via upload

---
+++ May2022/StockSpan/main.py
@@ -0,0 +1,43 @@
+from typing import List
+from common import *
+n = 10  # number of testcases
+class Solution:
+        def solutionGold(self,a):
+            n = len(a)
+            S = [0 for i in range(n+1)] 
+            st = []
+            st.append(0)
+            S[0] = 1
+    
+            for i in range(1, n):
+                while (len(st) > 0 and a[st[-1]] <= a[i]):
+                    st.pop()
+                S[i] = i + 1 if len(st) <= 0 else (i - st[-1])
+                st.append(i)
+            return S[0:n]
+        def soluser(self, num):
+            x = []
+            k = -2
+            return x
+
+for j in range(n):
+    testcaseNumber = j+1
+    try:
+        with open("testcases/ssp" + str(j+1) + ".txt") as f:
+            #num=int(f.read())
+            content =f.readlines()
+            num=[x.strip() for x in content]
+            #if num.length()<3 or num.length()>1000:
+                #print("Please enter an array of size in range of [3,1000]; testcase-",testcaseNumber)
+        #with open("testcases/ptf1" + str(j+1) + ".txt") as f:
+            #sum=int(f.read())
+    except:
+        print('Invalid testcase', testcaseNumber)
+        continue
+     
+    obj = Solution()
+    ans = obj.solutionGold(num)
+    userAns = obj.soluser(num)
+    check(ans, userAns, testcaseNumber)
+
+count_cases()
